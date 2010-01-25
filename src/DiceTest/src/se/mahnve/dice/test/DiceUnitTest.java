8ff1a67d12870ae66b06df9973fa1b965a67843d
Marcus Ahnve <marcus@ahnve.com>
2010-01-26 00:04:00 +0100

    shows why you can't test movement

--- src/DiceTest/src/se/mahnve/dice/test/DiceUnitTest.java
+++ src/DiceTest/src/se/mahnve/dice/test/DiceUnitTest.java
@@ -5,6 +5,8 @@ import static android.test.ViewAsserts.assertLeftAligned;
 import se.mahnve.dice.Dice;
 import se.mahnve.dice.R;
 import android.test.ActivityInstrumentationTestCase2;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -31,7 +33,11 @@ public class DiceUnitTest extends ActivityInstrumentationTestCase2<Dice> {
 	public void testResultShouldBeBelowButton() throws Exception {
 		assertLeftAligned(button, result);
 	}
-
+	
+/*	public void testShouldRollWhenShaken() throws Exception {
+		// SensorEvent sensor = new SensorEvent();
+	}
+*/
 	private void assertValidThrow(TextView textView) {
 		assertTrue("Should not be larger than 6", Integer.parseInt(textView.getText().toString()) < 7);
 		assertTrue("Should not be smaller than 1", Integer.parseInt(textView.getText().toString()) > 0);
