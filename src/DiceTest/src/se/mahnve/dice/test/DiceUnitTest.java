840601d878073d92989202a46e2d56761a3e80c7
Marcus Ahnve <marcus@ahnve.com>
2010-01-25 23:16:55 +0100

    refactored tests

--- src/DiceTest/src/se/mahnve/dice/test/DiceUnitTest.java
+++ src/DiceTest/src/se/mahnve/dice/test/DiceUnitTest.java
@@ -2,35 +2,43 @@ package se.mahnve.dice.test;
 
 import se.mahnve.dice.Dice;
 import se.mahnve.dice.R;
-import android.content.Intent;
+import android.hardware.SensorEvent;
 import android.test.ActivityInstrumentationTestCase2;
-import android.test.ActivityUnitTestCase;
 import android.test.UiThreadTest;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import static android.test.ViewAsserts.*;
+import static android.test.TouchUtils.*;
 
 public class DiceUnitTest extends ActivityInstrumentationTestCase2<Dice> {
+
+	private Button button;
+	private TextView result;
 
 	public DiceUnitTest() {
 		super("se.mahnve.dice", Dice.class);
 	}
 	
+	public void setUp() {
+		result = (TextView) getActivity().findViewById(R.id.result);
+		button = (Button) getActivity().findViewById(R.id.button);		
+	}
 	
-	@Override
-	protected void setUp() throws Exception {
-		super.setUp();
-		
+	public void testClickButtonShouldRollDice() throws Exception {
+		clickView(this, button);
+		assertValidThrow(result);
 	}
 
-
-	@UiThreadTest
-	public void testSomething() throws Exception {
-		assertNotNull(getActivity());
-		Button button = (Button) getActivity().findViewById(R.id.button);
-		button.performClick();
-		TextView textView = (TextView) getActivity().findViewById(R.id.result);
-		assert(Integer.parseInt((String) textView.getText()) > 0);
-		assert(Integer.parseInt((String) textView.getText()) < 7);
+	
+	public void testResultShouldBeBelowButton() throws Exception {
+		assertLeftAligned(button, result);
 	}
+
+	private void assertValidThrow(TextView textView) {
+		assertTrue("Should not be larger than 6", Integer.parseInt(textView.getText().toString()) < 7);
+		assertTrue("Should not be smaller than 1", Integer.parseInt(textView.getText().toString()) > 0);
+	}
+	
 
 }
